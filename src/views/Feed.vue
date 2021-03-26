42e198cc69e4046a077b9205348f68b1ad5d5b99
wggreen <wggreen.nss@gmail.com>
2021-03-26 11:07:55 -0500

    added search

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,9 +1,23 @@
 <template>
   <v-container>
-    <div v-for="meme in memes" :key="meme.id" class="py-5">
+    <v-row justify="center" class="mt-5">
+      <v-col :sm="12" :md="6">
+        <form @submit.prevent="addSearch">
+          <v-text-field
+            prepend-inner-icon="mdi-magnify"
+            color="teal darken-2"
+            rounded
+            outlined
+            v-model="inputVal"
+            label="Search"
+          />
+        </form>
+      </v-col>
+    </v-row>
+    <div v-for="meme in displayedMemes" :key="meme.id" class="py-5">
       <router-link :to="`/meme/${meme.id}`">
         <meme
-          class="mx-auto mt-6"
+          class="mx-auto"
           :top="meme.topText"
           :bottom="meme.bottomText"
           :imageURL="meme.imageURL"
@@ -21,12 +35,36 @@ export default {
   data() {
     return {
       memes: [],
+      inputVal: "",
+      searchTerm: "",
     };
+  },
+  methods: {
+    addSearch() {
+      this.searchTerm = this.inputVal;
+      this.$router.push({
+        path: "/feed",
+        query: { q: this.searchTerm },
+      });
+    },
   },
   components: {
     Meme,
   },
+  computed: {
+    displayedMemes() {
+      if (!this.searchTerm) return this.memes;
+
+      const normalizedSearchTerm = this.searchTerm.toUpperCase();
+      return this.memes.filter((m) => {
+        return m.normalized.includes(normalizedSearchTerm);
+      });
+    },
+  },
   mounted() {
+    this.inputVal = this.$route.query.q;
+    this.searchTerm = this.$route.query.q;
+
     db.collection("memes").onSnapshot((snap) => {
       const memes = snap.docs.map((doc) => {
         return {
